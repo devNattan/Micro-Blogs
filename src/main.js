da81640af4c1ac0454dbcf9d1161ed04f6edaba0
devNattan <natanaelhenrique.s.c@gmail.com>
2023-01-18 11:56:04 -0300

    finalizando aplicação

--- src/main.js
+++ src/main.js
@@ -12,9 +12,33 @@ const usersSelect = document.querySelector('#users-select');
 
 const USERS_API = 'https://dummyjson.com/users';
 
+fetch(USERS_API)
+  .then((response) => response.json())
+  .then((data) => {
+    const { users } = data;
+    fillUsersSelect(users);
+});
 
-usersSelect.addEventListener('change', () => {
+usersSelect.addEventListener('change', (e) => {
   clearPageData();
 
-  
+  const POSTS_API = `https://dummyjson.com/posts/user/${e.target.value}`;
+  fetch(POSTS_API)
+    .then((response) => response.json())
+    .then((data) => {
+        const  { posts }   = data;
+        console.log(posts);
+        fillPosts(posts);
+        const COMMENTS_API = `https://dummyjson.com/posts/${posts[0].id}/comments`;
+        return fetch(COMMENTS_API);
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      const  { comments } = data;
+      fillFeaturedPostComments(comments)
+    })
+    .catch((error) => {
+        fillErrorMessage('Erro ao recuperar informações');
+        console.log(error.message);
+    });
 });
